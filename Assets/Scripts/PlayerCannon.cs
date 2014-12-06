fe5aa33062c0924204beaf5367f4a403a8864bae
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-06 12:53:24 +0100

    Rotating cannon at init

---
+++ Assets/Scripts/PlayerCannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCannon : MonoBehaviour {
+
+	public float minAngle = 0f;
+	public float maxAngle = 180f;
+
+	public float shootingWobbling = 5f;
+
+	private Quaternion curQuat = new Quaternion(0f, 0f, 1f, -1.5f);
+	// Use this for initialization
+	void Start () {
+		transform.localRotation = curQuat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
